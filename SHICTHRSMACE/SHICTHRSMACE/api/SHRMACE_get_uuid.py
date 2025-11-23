638514dd77d6587f43bfdaadf924921e5db59e43
JNTMTMTM <15251865468@163.com>
2025-11-23 14:42:18 +0800

    V0.0.0 改进UUID获取错误处理：将默认值'N/A'替换为抛出SHRMACEException异常

--- SHICTHRSMACE/SHICTHRSMACE/api/SHRMACE_get_uuid.py
+++ SHICTHRSMACE/SHICTHRSMACE/api/SHRMACE_get_uuid.py
@@ -2,6 +2,7 @@
 import wmi
 import copy
 from ..SHRMACE_Data import SHRMACEResult
+from ..SHRMACE_ErrorBase import SHRMACEException
 
 def get_uuid() -> None:
     try:
@@ -9,4 +10,4 @@ def get_uuid() -> None:
         for system in c.Win32_ComputerSystemProduct():
             SHRMACEResult['WindowsUUID'] = copy.deepcopy(system.UUID)
     except:
-        SHRMACEResult['WindowsUUID'] = 'N/A'
+        raise SHRMACEException('SHRMACEException [ERROR.2000] unable to get uuid.')
